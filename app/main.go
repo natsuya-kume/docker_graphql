30e62e1c59d5123d2695f74a6c6309f2e8374944
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2022-02-07 01:36:39 +0900

    gqlgenの設定とgormでDB接続を実装

--- app/main.go
+++ app/main.go
@@ -1,29 +1,72 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/natsuya-kume/docker_graphql/app/graph/generated"
+	graph "github.com/natsuya-kume/docker_graphql/app/graph/resolver"
 )
 
 func main() {
+	db, err := gorm.Open("mysql", dbsn())
+	if err != nil {
+		log.Fatalln(err)
+	} else {
+		fmt.Println("DB接続成功しました！")
+	}
+
 	e := echo.New()
-
-	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
-	e.Use(middleware.Gzip())
-
+	e.Use(middleware.Recover())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{os.Getenv("CORS_ALLOW_ORIGIN")},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowOrigins: []string{"http://localhost:3000", "http://localhost:8080"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
 	}))
 
-	e.GET("/health", func(c echo.Context) error {
-		return c.NoContent(http.StatusOK)
+	e.GET("/", welcome())
+
+	graphqlHandler := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: db}}))
+
+	playgroundHandler := playground.Handler("GraphQL", "/query")
+
+	e.POST("/query", func(c echo.Context) error {
+		graphqlHandler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	e.GET("/playground", func(c echo.Context) error {
+		playgroundHandler.ServeHTTP(c.Response(), c.Request())
+		return nil
 	})
 
 	e.HideBanner = true
 	e.Logger.Fatal(e.Start(":3000"))
+}
+
+func dbsn() string {
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+}
+
+func welcome() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome!")
+	}
 }
